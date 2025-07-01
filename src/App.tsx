22958ef7e5b20827a3ba1451047bfd1376748985
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-07-01 20:42:05 -0300

    feat: Import CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Button } from './components/button'
 
-function App() {
+import './styles.css'
+
+export function App() {
   return (
-    <div>
+    <div className="container">
       <Button name="Criar" onClick={() => alert('Criar')} />
       <Button name="Editar" />
       <Button name="Remover" />
     </div>
   )
 }
-
-export default App
