6cbd03872c0d91c84223746da5bc1efbda950028
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-03 21:40:45 -0300

    refactor: restructure Button component and add styles
    - Removed old Button component implementation.
    - Created a new Button component with improved structure and styles.
    - Added CSS module for styling the Button component.
    - Updated package.json to include stylelint and its standard configuration.

--- src/components/button.tsx
+++
@@ -1,7 +0,0 @@
-type Props = React.ComponentProps<'button'> & {
-  name: string
-}
-
-export function Button({ name, onClick }: Props) {
-  return <button onClick={onClick}>{name}</button>
-}
---
+++ src/components/button/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.css"
+
+type Props = React.ComponentProps<'button'> & {
+  name: string
+}
+
+export function Button({ name, onClick }: Props) {
+  return (
+  <button className={styles.container} onClick={onClick}>
+    <span>
+      {name}
+    </span> 
+    </button>
+    )
+}
